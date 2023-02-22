810afce658cf3c5c9f5d05116e849d4401106f07
TRADEBOG <juan.miguel.jaramillo1984@gmail.com>
2023-02-22 13:57:55 -0500

    Corrección en la URL de la Imagen

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juanjaragavi.github.io',
-  base: '/juanjara',
+  base: '/',
   integrations: [tailwind(), react()],
   server: {
     watch: {
